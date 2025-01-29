a1b872f2123ea328cfdc7e4218f70ba456593375
hiagoat <paulinopedrohiago@gmail.com>
2025-01-29 10:08:18 -0300

    atualização lista1

---
+++ lista/lista1.js
@@ -0,0 +1,52 @@
+let a = 9
+let b = 69
+
+let soma = a + b
+let subtracao = a - b
+let multiplicacao = a * b
+let divisao = a / b
+let resto = a % b 
+console.log(soma)
+console.log(subtracao)
+console.log(multiplicacao)
+console.log(divisao)
+console.log(resto)
+
+/************************************************************* */
+
+let altura = 7
+let largura = 9
+
+let mult = altura*largura
+console.log(mult)
+
+/************************************************************* */
+
+let num = 8
+let nume = 2
+
+let conta = 8*2
+let conta2 = 8/2
+
+console.log(conta)
+console.log(conta2)
+
+
+/************************************************************** */
+
+let num1 = 4
+let num2 = 5
+let num3 = 8
+ 
+let mult1 = 4*5*8
+let media = mult1/3
+
+console.log(mult1)
+console.log(media)
+
+/************************************************************** */
+
+let celsius = 9
+
+let f = celsius * 9/5 + 32
+console.log(f)
