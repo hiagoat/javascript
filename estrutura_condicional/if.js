e3af9719910be067d6d3c4bf06b24a0ae1f1c084
hiagoat <paulinopedrohiago@gmail.com>
2025-02-04 09:22:45 -0300

    atualização lista3

---
+++ estrutura_condicional/if.js
@@ -0,0 +1,28 @@
+let idade = prompt("Quase sua Idade?")
+console.log("Sua idade é", idade)
+
+if (idade >= 18) {
+    console.log("Você é maior de idade")
+} else {
+    console.log("Você é menor de idade")
+}
+console.log("-------------------------------------------------------")
+// Verificando se um número é impar ou par //
+let numero = prompt("Escolha um número:")
+let resultado = numero % 2 //resto
+if (resultado == 0) {
+    console.log("O número é par")
+} else {
+    console.log("O número é impar")
+}
+console.log("---------------------------------------")
+// Avaliação do aluno
+
+let nota1 = prompt("Qual a primeira nota:")
+let nota2 = prompt("Qual a segunda nota:")
+let total1 = (nota1 + nota2) / 2
+if (total1 >= 7) {
+    console.log("Aprovado")
+}else {
+    console.log("Reprovado")
+}
---
+++ lista/lista3.js
@@ -0,0 +1,58 @@
+let idade = 16
+if (idade >=18 ){
+    console.log("Você é maior de idade.")
+}else {
+    console.log("Você é menor de idade")
+}
+
+// Exercicio 2
+
+let numero = 8
+let resultado = numero % 2
+if (resultado == 0 ){
+    console.log("Seu número é par")
+}else {
+    console.log("Seu número é impar")
+}
+
+// Exercicio 3
+
+let not1 = 8
+if (not1 >= 7){
+    console.log("Aprovado")
+}else {
+    console.log("Reprovado")
+}
+
+// Exercicio 4
+
+let ida = 20
+if (ida >=18 ){
+    console.log("Entrada permitida")
+}else {
+    console.log("Entrada proibida")
+}
+
+// Exercicio 5
+let senha = 1234
+if (senha == 1234 ){
+    console.log("Senha correta")
+}else {
+    console.log("Senha incorreta")
+}
+
+// Exercicio 6
+let temperatura = 28
+if (temperatura > 30 ){
+    console.log("Está quente")
+}else {
+    console.log("Está frio")
+}
+
+// Exercicio 7
+let number = -8
+if (number >= 0 ){
+    console.log("Número positivo")
+}else {
+    console.log("Número negativo")
+}
