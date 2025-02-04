b26eb641dc4f11ccd095b3169f8f96d849755ad4
hiagoat <paulinopedrohiago@gmail.com>
2025-02-04 09:28:12 -0300

    Uptade lista 3

--- lista/lista3.js
+++ lista/lista3.js
@@ -1,5 +1,5 @@
-let idade = 16
-if (idade >=18 ){
+let idade9 = 16
+if (idade9 >=18 ){
     console.log("Você é maior de idade.")
 }else {
     console.log("Você é menor de idade")
@@ -7,9 +7,9 @@ if (idade >=18 ){
 
 // Exercicio 2
 
-let numero = 8
-let resultado = numero % 2
-if (resultado == 0 ){
+let numero1 = 8
+let resultado5 = numero % 2
+if (resultado5 == 0 ){
     console.log("Seu número é par")
 }else {
     console.log("Seu número é impar")
@@ -17,8 +17,8 @@ if (resultado == 0 ){
 
 // Exercicio 3
 
-let not1 = 8
-if (not1 >= 7){
+let not2 = 8
+if (not2 >= 7){
     console.log("Aprovado")
 }else {
     console.log("Reprovado")
@@ -26,32 +26,32 @@ if (not1 >= 7){
 
 // Exercicio 4
 
-let ida = 20
-if (ida >=18 ){
+let ida1 = 20
+if (ida1 >=18 ){
     console.log("Entrada permitida")
 }else {
     console.log("Entrada proibida")
 }
 
 // Exercicio 5
-let senha = 1234
-if (senha == 1234 ){
+let senha1 = 1234
+if (senha1 == 1234 ){
     console.log("Senha correta")
 }else {
     console.log("Senha incorreta")
 }
 
 // Exercicio 6
-let temperatura = 28
-if (temperatura > 30 ){
+let temperatura1 = 28
+if (temperatura1 > 30 ){
     console.log("Está quente")
 }else {
     console.log("Está frio")
 }
 
 // Exercicio 7
-let number1 = -8
-if (number1 >= 0 ){
+let number2 = -8
+if (number2 >= 0 ){
     console.log("Número positivo")
 }else {
     console.log("Número negativo")
