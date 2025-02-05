3eb50305c39283f854a9f867714af440f8c90202
hiagoat <paulinopedrohiago@gmail.com>
2025-02-05 10:08:58 -0300

    upload lista 5

--- lista/lista5.js
+++ lista/lista5.js
@@ -1,6 +1,6 @@
-let idade = prompt("Quase sua Idade?")
-let carteira = true
-if (idade >= 18 && carteira == true){
+let idade1 = prompt("Quase sua Idade?")
+let carteira1 = true
+if (idad1e >= 18 && carteira == true){
     console.log ("Você pode dirigir")
 } else {
     console.log("Você não pode dirigir")
@@ -8,9 +8,9 @@ if (idade >= 18 && carteira == true){
 
 // Exercicio 2
 
-let anos = prompt("Quantos anos você tem na empresa?")
-let projetos = prompt("Quantos projetos você já realizou?")
-if (anos >= 5 || projetos >= 10 ){
+let anos1 = prompt("Quantos anos você tem na empresa?")
+let projeto1s = prompt("Quantos projetos você já realizou?")
+if (anos1 >= 5 || projetos1 >= 10 ){
     console.log ("Você está elegível para promoção")
 } else {
     console.log("Você não está elegível para promoção")
@@ -18,8 +18,8 @@ if (anos >= 5 || projetos >= 10 ){
 
 // Exercicio 3
 
-let acesso = prompt("Qual é a sua idade?")
-if (acesso >= 18 && acesso <=30){
+let acesso1 = prompt("Qual é a sua idade?")
+if (acesso1 >= 18 && acesso1 <=30){
     console.log("Você pode entrar!")
 }else {
     console.log("Você não pode entrar!")
@@ -28,9 +28,9 @@ if (acesso >= 18 && acesso <=30){
 
 // Exercicio 4
 
-let pessoa = prompt("Diga sua idade")
-let experiencia = true
-if (pessoa >= 21 && experiencia == true){
+let pessoa1 = prompt("Diga sua idade")
+let experiencia1 = true
+if (pessoa1 >= 21 && experiencia1 == true){
     console.log ("Você foi aceito para vaga!")
 }else {
     console.log("Você não foi aceito")
@@ -39,9 +39,9 @@ if (pessoa >= 21 && experiencia == true){
 
 // Exercicio 5
 
-let usuario = prompt("Digite seu nome")
-let senha = prompt ("Digite sua senha")
-if (senha = "admin" && senha == 1234){
+let usuario1 = prompt("Digite seu nome")
+let senha1 = prompt ("Digite sua senha")
+if (senha1 = "admin" && senha1 == 1234){
     console.log ("Login bem-sucedido!")
 }else {
     console.log("Nome de usuário ou senha incorretos")
@@ -49,18 +49,18 @@ if (senha = "admin" && senha == 1234){
 
 
 // Exercicio 6
-let numero = prompt("Digite um número:")
-if (numero >= 10  && numero <= 20){
+let numero1 = prompt("Digite um número:")
+if (numero1 >= 10  && numero1 <= 20){
     console.log("O número está dentro do intervalo entre 10 e 20")
-}else if (numero >= 30 && numero <= 50) {
+}else if (numero1 >= 30 && numero1 <= 50) {
     console.log("O número está dentro do intervalo entre 30 e 50")
 }else {
     console.log("ele não está dentro do intervalo de 10 e 20 nem 30 e 50")
 }
 
 // Exercicio 7 
-let num = prompt("Me diga um número")
-if (num <10 || num > 100 && num == 50){
+let num1 = prompt("Me diga um número")
+if (num1 <10 || num1 > 100 && num1 == 50){
     console.log("O número é aceito")
 }else {
     console.log("O número não é aceito")
