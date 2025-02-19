61297d02ad20917fe7e42ae991a4c4e72658315c
hiagoat <paulinopedrohiago@gmail.com>
2025-02-19 13:15:13 -0300

    Uptade lista13

--- lista/lista13.js
+++ lista/lista13.js
@@ -1,14 +1,59 @@
 let saudacao = function(nome){
-    console.log("Oi", nome)
+    console.log("Ola", nome)
 }
-saudacao("Pedro Hiago")
 
-//Exercicio 2
-let soma = function(a,b) {
-    let a = Number(prompt("Digite um número"));
-    let b = Number(prompt("Digite o segundo número"));
-    return resultado
+saudacao (prompt("Qual seu nome?"))
+
+// Exercicio 2
+let soma = function(numero,numero){
+    console.log(numero+numero1)
 }
-console.log(soma()) 
+
+let numero = Number(prompt("Digite um numero"))
+let numero1 = Number(prompt("Digite outro numero"))
+
+soma(numero,numero1)
 
 
+// Exercicio 3
+let retangulo = function(num1,num2){
+    console.log(num1*num2)
+}
+
+num1 = Number(prompt("Digite um numero"))
+num2 = Number(prompt("Digite outro numero"))
+
+retangulo(num1,num2)
+
+
+// Exercicio 4
+let variavel = (var1) => var1*2
+let var1 = Number(prompt("Escolha um numero:"))
+
+console.log(num(var1))
+
+
+// Exercicio 5
+let arrow = (aleatorio) => aleatorio*aleatorio
+let aleatorio = Number(prompt("Escolha um numero:"))
+
+console.log(arrow(aleatorio ))
+
+// Exercicio 6
+let verificacao = (idade) => idade >= 18 ? "Maior de idade" : "Menor de idade"
+
+let idade = Number(prompt("Qual sua idade?"))
+console.log(verificacao(idade))
+
+// Exercicio 7
+let media = (nota1, nota2, nota3) => {
+    let nota= (nota1+nota2+nota3)
+    let result = (nota/3)
+    return (resultaldo)
+}
+
+let nota1 = Number(prompt("Qual sua primeira nota?"))
+let nota2 = Number(prompt("Qual sua segunda nota?"))
+let nota3 = Number(prompt("Qual sua terceira nota?"))
+
+console.log(media(nota1,nota2,nota3))
