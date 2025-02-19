6fc9da976829162b373930f4d7ba6dd78df34d00
hiagoat <paulinopedrohiago@gmail.com>
2025-02-19 08:13:49 -0300

    Uptade lista11

--- lista/lista11.js
+++ lista/lista11.js
@@ -14,17 +14,56 @@ let cor = ["Vermelho", "Preto", "Roxo"]
 for (let c = 0; c < cor.push(); c++) {
     console.log(cor[c]);
 }
-console.log("Adicionando no final")
-cor.push("Rosa")
-console.log(cor)
+console.log("Adicionando no final");
+cor.push("Rosa");
+console.log(cor);
+////////////Exercicio 3
+let cores = ["Vermelho", "Azul", "Verde"];
+cores.push("Amarelo");
+console.log("Array atualizado:");
+for (let i = 0; i < cores.length; i++) {
+    console.log(cores[i]);
+}
 
 ////////////Exercicio 4
 console.log("---------------------------------------------------------")
-let numero = [5 , 7, 8, 6]
-numero.pop
-for (let n = 0; n < numero.pop(); n++) {
-    console.log(numero [n]);
+let numeros = [10, 20, 30, 40];
+numeros.pop();
+console.log("Array atualizado:");
+for (let i = 0; i < numeros.length; i++) {
+    console.log(numeros[i]);
 }
-console.log("Removido do final")
-cor.pop()
-console.log(numero)
+
+////////////Exercicio 5
+console.log("---------------------------------------------------------")
+let cidades = ["São Paulo", "Rio de Janeiro"];
+cidades.unshift("Curitiba");
+console.log("Array atualizado:");
+for (let i = 0; i < cidades.length; i++) {
+    console.log(cidades[i]);
+}
+
+////////////Exercicio 6
+console.log("---------------------------------------------------------")
+let animais = ["Cachorro", "Gato", "Pássaro"];
+animais.shift();
+console.log("Array atualizado:");
+for (let i = 0; i < animais.length; i++) {
+    console.log(animais[i]);
+}
+////////////Exercicio 7
+console.log("---------------------------------------------------------")
+let carros = ["Ferrari", "BMW", "Toyota", "Honda"];
+console.log("Quantidade de carros:", carros.length);
+
+////////////Exercicio 8
+console.log("---------------------------------------------------------")
+let numeros1 = [];
+numeros1.push(5);
+numeros1.push(10);
+numeros1.push(15);
+numeros1.unshift(20);
+numeros1.pop();
+numeros1.shift();
+console.log("Array final:", numerosArray);
+console.log("Tamanho do array:", numerosArray.length);
