089d62b0d3bfa5d3e48bf8d9725fa950e59fbeec
hiagoat <paulinopedrohiago@gmail.com>
2025-04-29 14:18:41 -0300

    Update loja

---
+++ drag_and_drop/drag_and_drop/projeto/script.js
@@ -0,0 +1,21 @@
+const tarefas = document.querySelectorAll(".tarefa")
+const colunas = document.querySelectorAll(".tarefas")
+
+tarefas.forEach(tarefa => {
+    tarefa.addEventListener("dragstart", () => {
+        tarefa.classList.add("arrastando")
+    })
+    tarefa.addEventListener("dragend", () => {
+        tarefa.classList.remove("arrastando")
+    })
+})
+
+colunas.forEach(coluna => {
+    coluna.addEventListener("dragover", (e) => {
+        e.preventDefault()
+        const tarefaArrastando = document.querySelector(".arrastando")
+        if (tarefaArrastando) {
+            coluna.appendChild(tarefaArrastando)
+        }
+    })
+})
